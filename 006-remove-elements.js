ea08b0b62c6ac513c28a22e5895d3054158993de
vroksiroy <royvroksi@gmail.com>
2026-04-25 17:15:01 +0530

    Add 006-remove-elements.js

---
+++ 006-remove-elements.js
@@ -0,0 +1,15 @@
+var removeElement = function(nums, val) {
+    let x = 0
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] !== val){
+            nums[x] = nums[i]
+            x = x+1
+        }
+    }
+    return x
+};
+
+let nums = [3,2,2,3]
+let val = 3
+let result = removeElement(nums, val);
+console.log(result);
